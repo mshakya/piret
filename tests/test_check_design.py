54667534d34a659313079127cf0178c7d1cf02c6
Migun Shakya <microbeatic@gmail.com>
2017-02-24 11:59:01 -0700

    added more test

--- tests/test_check_design.py
+++ tests/test_check_design.py
@@ -11,14 +11,27 @@ class TestCheckTab(unittest.TestCase):
 
     def test_false(self):
         """Test if false."""
-        check_design_csv = CheckDesign("test_experimental_design.csv")
-        self.assertFalse(check_design_csv.check_tab)
+        check_design_csv = CheckDesign()
+        self.assertFalse(check_design_csv.check_tab("__init__.py"))
 
     def test_true(self):
         """Test if true."""
-        check_design_tsv = CheckDesign("test_experimental_design.txt")
-        self.assertTrue(check_design_tsv.check_tab)
+        check_design_tsv = CheckDesign()
+        self.assertTrue(check_design_tsv.check_tab("test_experimental_design.txt"))
 
+
+class TestCheckHeader(unittest.TestCase):
+    """Unittest testcase."""
+
+    def test_false(self):
+        """Test if False."""
+        check_design = CheckDesign()
+        self.assertFalse(check_design.check_header("test_experimental_design.csv"))
+
+    def test_true(self):
+        """Test if True."""
+        check_design = CheckDesign()
+        self.assertTrue(check_design.check_header("test_experimental_design.txt"))
 
 if __name__ == '__main__':
     unittest.main()
