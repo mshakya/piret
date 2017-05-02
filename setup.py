d0178b380a2fca91109dc2f5c1f8a538adae136b
Migun Shakya <microbeatic@gmail.com>
2017-05-02 11:33:24 -0600

    setup.py function

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pypiret",
+    version='0.1.0',
+    description="RNAseq pipeline",
+    author="Migun Shakya",
+    author_email="migun@lanl.gov",
+    url="https://github.com/mshakya/pypiret",
+    install_requires=open("requirements.txt").read().splitlines(),
+    packages=find_packages(),
+    license="Apache License 2.0",
+    platforms="Posix; MacOS X",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Bioinformatics",
+    ],
+)
