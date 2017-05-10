0debcdba0d0d36efe17411b3e6a39cfe9acfd1bc
Migun Shakya <microbeatic@gmail.com>
2017-05-09 22:13:38 -0600

    importing dependencies

--- pypiret/__init__.py
+++ pypiret/__init__.py
@@ -1,8 +1,8 @@
 """Import important functions."""
 
-from file_exist import file_exist
 from Checks.Design import CheckDesign
 from Runs import FastQC
 from Runs import Map
 from Runs import Summ
+from Checks.Dependencies import CheckDependencies
 from initialize import Initialize
