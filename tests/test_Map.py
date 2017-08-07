2118c8a2d7bf6e4065988cf4e0bf78e42ce7fa92
Migun Shakya <microbeatic@gmail.com>
2017-08-07 17:27:21 -0600

    created test for CreateSplice

--- tests/test_Map.py
+++ tests/test_Map.py
@@ -48,6 +48,46 @@ class TestGFF2GTF(unittest.TestCase):
         """Remove created files and directories."""
         shutil.rmtree("tests/test_gff2gtf/")
 
+
+class TestCreateSplice(unittest.TestCase):
+    """Unittest testcase."""
+
+    def setUp(self):
+        """Setting up the directory."""
+        if os.path.exists("tests/test_createsplice") is False:
+            os.makedirs("tests/test_createsplice")
+
+    def test_splice_creation(self):
+        """Test if CreateSplice works."""
+        luigi.interface.build([Map.GFF2GTF(gff_file="tests/data/test_prok.gff",
+                                           bindir="bin",
+                                           workdir="tests/test_createsplice"),
+                               Map.CreateSplice(gff_file="tests/data/test_prok.gff",
+                                                bindir="bin",
+                                                workdir="tests/test_createsplice")],
+                              local_scheduler=True)
+        self.assertTrue(os.path.exists("tests/test_createsplice/test_prok.splice"))
+
+    def test_splices_creation(self):
+        """Test if GFF2GTF works for multiple gffs."""
+        luigi.interface.build([Map.GFF2GTF(gff_file="tests/data/test_prok.gff,tests/data/eukarya_test.gff3",
+                                           bindir="bin",
+                                           workdir="tests/test_createsplice"),
+                               Map.CreateSplice(gff_file="tests/data/test_prok.gff,tests/data/eukarya_test.gff3",
+                                                bindir="bin",
+                                                workdir="tests/test_createsplice")],
+                              local_scheduler=True)
+
+        self.assertTrue(os.path.exists("tests/test_createsplice/test_prok.splice"))
+        self.assertTrue(os.path.exists("tests/test_createsplice/eukarya_test.splice"))
+        with open("tests/test_createsplice/eukarya_test.splice") as pg:
+            first_line_second_column = pg.readline().split("\t")[1]
+            self.assertEqual(first_line_second_column, "220")
+
+    # def tearDown(self):
+    #     """Remove created files and directories."""
+    #     shutil.rmtree("tests/test_createsplice/")
+
 # class Test
 if __name__ == '__main__':
     unittest.main()
