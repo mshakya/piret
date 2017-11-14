1ace6ebeb7d71279532b3d27533275c1dd86405d
Migun Shakya <microbeatic@gmail.com>
2017-11-13 18:17:00 -0700

    adding paths to import piret modules correctly

--- pypiret/Checks/__init__.py
+++ pypiret/Checks/__init__.py
@@ -1,4 +1,8 @@
 """Importing."""
+import sys, os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(dir_path)
 from Design import CheckDesign
 from Fasta import CheckFasta
 from Dependencies import CheckDependencies
--- pypiret/__init__.py
+++ pypiret/__init__.py
@@ -1,5 +1,9 @@
 """Import important functions."""
 
+import sys, os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
+
 from Checks.Design import CheckDesign
 from Checks.Fasta import CheckFasta
 from Runs import FastQC
