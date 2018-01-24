67f3e6a05938b5a327210e7c758d3ed9bc457dc2
Migun Shakya <microbeatic@gmail.com>
2018-01-24 13:42:00 -0700

    integrating DGE

--- pypiret/Runs/DGE.py
+++ pypiret/Runs/DGE.py
@@ -56,36 +56,36 @@ class EdgeR(ExternalProgramTask):
         return {'PATH': self.bindir + "/../scripts/" + ":" + os.environ["PATH"]}
 
 
-class EdgeR(ExternalProgramTask):
-    """Find DGE using EdgeR."""
+# class EdgeR(ExternalProgramTask):
+#     """Find DGE using EdgeR."""
 
-    exp_design = luigi.Parameter()
-    p_value = luigi.FloatParameter()
+#     exp_design = luigi.Parameter()
+#     p_value = luigi.FloatParameter()
 
-    def requires(self):
-        """Require count file to be present."""
-        return RefFile(os.path.join(self.workdir, "featureCounts", "CDS.count"))
+#     def requires(self):
+#         """Require count file to be present."""
+#         return RefFile(os.path.join(self.workdir, "featureCounts", "CDS.count"))
 
-    def output(self):
-        """Expected output of DGE using EdgeR."""
-        edger_dir = self.workdir + "/edgeR/" + self.kingdom
-        return LocalTarget(edger_dir + "/" + "CPM.csv")
+#     def output(self):
+#         """Expected output of DGE using EdgeR."""
+#         edger_dir = self.workdir + "/edgeR/" + self.kingdom
+#         return LocalTarget(edger_dir + "/" + "CPM.csv")
 
-    def program_args(self):
-        """Run EdgeR."""
-        fcount_dir = self.workdir + "/featureCounts"
-        edger_dir = self.workdir + "/edgeR/" + self.kingdom
-        if not os.path.exists(edger_dir):
-            os.makedirs(edger_dir)
-        for root, dirs, files in os.walk(fcount_dir):
-            for file in files:
-                if file.endswith("count"):
-                    return ["Rscript", self.bindir + "/../scripts/EdgeR",
-                            "-r", os.path.join(root, file),
-                            "-e", self.exp_design,
-                            "-p", self.p_value,
-                            "-o", edger_dir]
+#     def program_args(self):
+#         """Run EdgeR."""
+#         fcount_dir = self.workdir + "/featureCounts"
+#         edger_dir = self.workdir + "/edgeR/" + self.kingdom
+#         if not os.path.exists(edger_dir):
+#             os.makedirs(edger_dir)
+#         for root, dirs, files in os.walk(fcount_dir):
+#             for file in files:
+#                 if file.endswith("count"):
+#                     return ["Rscript", self.bindir + "/../scripts/EdgeR",
+#                             "-r", os.path.join(root, file),
+#                             "-e", self.exp_design,
+#                             "-p", self.p_value,
+#                             "-o", edger_dir]
 
-    def program_environment(self):
-        """Environmental variables for this program."""
-        return {'PATH': self.bindir + "/../scripts/" + ":" + os.environ["PATH"]}
+#     def program_environment(self):
+#         """Environmental variables for this program."""
+#         return {'PATH': self.bindir + "/../scripts/" + ":" + os.environ["PATH"]}
