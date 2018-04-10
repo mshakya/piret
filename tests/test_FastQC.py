e86aab0c7a518012322d58763ba7a2727b3cbfae
Migun Shakya <microbeatic@gmail.com>
2018-04-10 10:34:51 -0600

    updated with correct number

--- tests/test_FastQC.py
+++ tests/test_FastQC.py
@@ -25,27 +25,27 @@ class TestFaQC(unittest.TestCase):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         bindir = os.path.abspath(os.path.join(dir_path, '..', 'bin'))
         luigi.interface.build([FastQC.PairedRunQC(fastqs=["tests/data/fastqs/BTT_test15_R1.fastq",
-                                                       "tests/data/fastqs/BTT_test15_R1.fastq"],
-                                                sample="samp1",
-                                                numCPUs=1,
-                                                qc_outdir="test_faqc",
-                                                bindir=bindir,
-                                                faqc_min_L=50,
-                                                n_cutoff=5)],
-                                                local_scheduler=True,
-                                                workers=1)
-        
+                                                          "tests/data/fastqs/BTT_test15_R1.fastq"],
+                                                  sample="samp1",
+                                                  numCPUs=1,
+                                                  qc_outdir="test_faqc",
+                                                  bindir=bindir,
+                                                  faqc_min_L=50,
+                                                  n_cutoff=5)],
+                              local_scheduler=True,
+                              workers=1)
+       
 
     def test_fq_out(self):
         """Test if fq has expected line number"""
         fq_lines = self.file_len("test_faqc/samp1.1.trimmed.fastq")
-        assert(fq_lines) == 188420
+        assert(fq_lines) == 189508
 
     def test_output(self):
         """Test if 5 files are generated as output"""
-        filenames =  [x[2] for x in os.walk("test_faqc")][0]
+        filenames = [x[2] for x in os.walk("test_faqc")][0]
         assert len(filenames) == 5
-    
+   
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree('test_faqc')
