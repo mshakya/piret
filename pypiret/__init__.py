8ae152b0e8d5fd27e8df339be8e706324c42f61b
Migun Shakya <microbeatic@gmail.com>
2019-05-01 15:14:08 -0600

    adding script dir to $PATH variable

--- pypiret/__init__.py
+++ pypiret/__init__.py
@@ -1,16 +1,20 @@
 """Import important functions."""
 
-import sys, os
+import sys
+import os
+import shutil
+import subprocess
 dir_path = os.path.dirname(os.path.realpath(__file__))
+script_dir = os.path.abspath(os.path.join(dir_path, "../scripts"))
 sys.path.append(dir_path)
-
+os.environ["PATH"] += ":" + script_dir
 from Checks.Design import CheckDesign
 from Checks.Fasta import CheckFasta
+from Checks.Dependencies import CheckDependencies
 from Runs import FaQC
 from Runs import srna
 from Runs import Map
 from Runs import Summ
 from Runs import DGE
 from Workflows import SingleSeq
-from Checks.Dependencies import CheckDependencies
 from initialize import Initialize
