3cf1a653759fd4267a3c639f74715487ab8852e5
Migun Shakya <microbeatic@gmail.com>
2019-05-01 22:44:37 -0600

    removing importing packages here

--- pypiret/__init__.py
+++ pypiret/__init__.py
@@ -8,13 +8,13 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 script_dir = os.path.abspath(os.path.join(dir_path, "../scripts"))
 sys.path.append(dir_path)
 os.environ["PATH"] += ":" + script_dir
-from Checks.Design import CheckDesign
-from Checks.Fasta import CheckFasta
-from Checks.Dependencies import CheckDependencies
-from Runs import FaQC
-from Runs import srna
-from Runs import Map
-from Runs import Summ
-from Runs import DGE
-from Workflows import SingleSeq
-from initialize import Initialize
+# from Checks.Design import CheckDesign
+# from Checks.Fasta import CheckFasta
+# from Checks.Dependencies import CheckDependencies
+# from Runs import FaQC
+# from Runs import srna
+# from Runs import Map
+# from Runs import Summ
+# # from Runs import DGE
+# from Workflows import SingleSeq
+# from initialize import Initialize
