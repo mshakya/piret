af2218a8793793153b8be5dc13e808f226365111
Migun Shakya <microbeatic@gmail.com>
2019-11-18 10:41:23 -0700

    making it work all the way till pathway downloads

--- piret/pathways/opaver.py
+++ piret/pathways/opaver.py
@@ -1,12 +1,8 @@
 #! /usr/bin/env python
 
-"""Luigi Tasks to perform various RNA seq functions, from Mapping to Counting.
-
-Mapping is done using hisat2 and counting is done using featurecounts
-and stringtie
+"""Luigi Tasks to call opaver functions
 """
 
-from __future__ import print_function
 import os
 import luigi
 import sys
@@ -16,13 +12,12 @@ lib_path = os.path.abspath(os.path.join(dir_path, '..'))
 sys.path.append(lib_path)
 from luigi.contrib.external_program import ExternalProgramTask
 from luigi import LocalTarget
-opaver_dir = os.path.abspath(os.path.join(dir_path, "../../thirdparty/omics-pathway-viewer/scripts"))
-from plumbum.cmd import perl
+from plumbum.cmd import perl, time
 import pandas as pd
 from sys import stderr, exit
 import logging
 import glob
-# from Bio import SeqIO
+from piret.qc.FaQC import RefFile
 
 
 class RunOpaver(luigi.Task):
@@ -34,34 +29,38 @@ class RunOpaver(luigi.Task):
 
     def requires(self):
         """de."""
+        emapper_file = os.path.join(self.workdir, "processes", "emapper",
+                                    self.kingdom,
+                                    "emapper.emapper.annotations")
+        return RefFile(emapper_file)
 
     def output(self):
         """Local target."""
-        if self.method == "edgeR":
+        dge_dir = os.path.join(self.workdir, "processes", self.method,
+                               self.kingdom, "CDS")
+        dge_files = [f for f in glob.glob(dge_dir + "**/*sig.csv",
+                     recursive=True)]
+        for file in dge_files:
+            cond_fdr = os.path.basename(file).split("__et.csv")[0]
             opaver_outfile = os.path.join(self.workdir, "processes", "opaver",
-                                          self.kingdom, "edgeR", "opaver.tsv")
-        elif self.method == "DESeq2":
-            opaver_outfile = os.path.join(self.workdir, "processes", "opaver",
-                                          self.kingdom, "DESeq2", "opaver.tsv")
-        return LocalTarget(opaver_outfile)
+                                                self.kingdom, self.method,
+                                                cond_fdr, "opaver.tsv")
+            return LocalTarget(opaver_outfile)
 
     def run(self):
         """."""
         if self.method == "edgeR":
-            self.prep_opaver(method="edgeR")
-            # self.run_opaver(method="edgeR")
+            opaver_files = self.prep_opaver(method="edgeR")
+            self.run_opaver(method="edgeR", opavers=opaver_files)
         elif self.method == "DESeq2":
-            self.prep_opaver(method="DESeq2")
-            # self.run_opaver(method="DESeq2")
+            opaver_files = self.prep_opaver(method="DESeq2")
+            self.run_opaver(method="DESeq2", opavers=opaver_files)
 
     def prep_opaver(self, method):
-        opaver_outdir = os.path.join(self.workdir, "processes", "opaver",
-                                     self.kingdom, method)
-        if os.path.exists(opaver_outdir) is False:
-            os.makedirs(opaver_outdir)
+        out_files = []
         dge_dir = os.path.join(self.workdir, "processes", method,
                                self.kingdom, "CDS")
-        dge_files = [f for f in glob.glob(dge_dir + "**/*et.csv",
+        dge_files = [f for f in glob.glob(dge_dir + "**/*sig.csv",
                      recursive=True)]
         emapper_file = os.path.join(self.workdir, "processes", "emapper",
                                     self.kingdom,
@@ -74,53 +73,66 @@ class RunOpaver(luigi.Task):
         emap1.columns = emap1.iloc[0]
         emap2 = emap1.drop(0).set_index('#query_name')
         if os.path.exists(emapper_file) is False:
-            sys.exit("emapper file is not present! No pathway analysis")
+            sys.exit("emapper output is not found! No pathway analysis for you!")
         else:
             for file in dge_files:
                 dge_df = pd.read_csv(file, sep=",", index_col=0)
-            if method == "edgeR":
-                dge_df = dge_df.drop(["Chr", "Start", "End",
+                if method == "edgeR":
+                    cond_fdr = os.path.basename(file).split("__et.csv")[0]
+                    opaver_outdir = os.path.join(self.workdir, "processes",
+                                              "opaver", self.kingdom,
+                                              method, cond_fdr)
+                    if os.path.exists(opaver_outdir) is False:
+                        os.makedirs(opaver_outdir)
+                    dge_df = dge_df.drop(["Chr", "Start", "End",
                                       "Strand", "Length", "logCPM"],
                                        axis=1)
-                print(dge_df)
-                # merge the two file.
-                opaver_df = pd.merge(dge_df, emap2, left_on="Geneid",
+                    # merge the two file.
+                    opaver_df = pd.merge(dge_df, emap2, left_on="Geneid",
                                      right_index=True,
                                      how="inner").loc[:, ['Geneid', 'KEGG_ko',
                                                           'logFC',
                                                           'PValue']]
-                opaver_df.KEGG_ko = opaver_df.KEGG_ko.apply(self.remove_ko)
-                opaver_df.to_csv(os.path.join(opaver_outdir, "opaver.tsv"),
-                                 sep="\t", index=False)
-            elif method == "DESeq2":
-                dge_df = dge_df.drop(["baseMean", "lfcSE",
+                    opaver_df.KEGG_ko = opaver_df.KEGG_ko.apply(self.remove_ko)
+                    opaver_df.to_csv(os.path.join(opaver_outdir, "opaver.tsv"),
+                                     sep="\t", index=False)
+                    out_files.append(os.path.join(opaver_outdir, "opaver.tsv"))
+                elif method == "DESeq2":
+                    cond_fdr = os.path.basename(file).split("__et.csv")[0]
+                    opaver_outdir = os.path.join(self.workdir, "processes", "opaver",
+                                            self.kingdom, method,
+                                            cond_fdr)
+                    if os.path.exists(opaver_outdir) is False:
+                        os.makedirs(opaver_outdir)
+                    dge_df = dge_df.drop(["baseMean", "lfcSE",
                                       "stat"], axis=1)
-                opaver_df = pd.merge(dge_df, emap2, left_index=True,
+                    opaver_df = pd.merge(dge_df, emap2, left_index=True,
                                      right_index=True,
                                      how="inner").loc[:, ['KEGG_ko',
                                                           'log2FoldChange',
                                                           'pvalue', 'padj']]
-                opaver_df.KEGG_ko = opaver_df.KEGG_ko.apply(self.remove_ko)
-                opaver_df.to_csv(os.path.join(opaver_outdir, "opaver.tsv"),
+                    opaver_df.KEGG_ko = opaver_df.KEGG_ko.apply(self.remove_ko)
+                    opaver_df.to_csv(os.path.join(opaver_outdir, "opaver.tsv"),
                                  sep="\t")
+                    out_files.append(os.path.join(opaver_outdir, "opaver.tsv"))
+        return out_files
 
     def remove_ko(self, x):
         "removes a ko string"
-        if x != "nan":
+        if x != "nan" and x not in ["01100", "01120", "01130"]:
             return str(x).replace("ko:", "")
         else:
             return None
 
-    def run_opaver(self, method):
+    def run_opaver(self, method, opavers):
         """ Run opaver.pl script."""
-        in_file = os.path.join(self.workdir, "processes", "opaver", self.kingdom,
-                               method, "opaver.tsv")
-
-        out_folder = os.path.join(self.workdir, "processes", "opaver", self.kingdom,
-                                  method, "path_ids")
-        if os.path.exists(out_folder) is False:
-            os.makedirs(out_folder)
-        opaver_pl = [os.path.join(opaver_dir, "opaver.pl"), "--tran", in_file,
+        for opav_file in opavers:
+            out_folder = os.path.dirname(opav_file)
+            if os.path.exists(out_folder) is False:
+                os.makedirs(out_folder)
+            opaver_pl = ["opaver.pl", "--tran", opav_file,
                      "-o", out_folder]
-        perl[opaver_pl]()
+            time[opaver_pl]()
+            opaver_r = ["opaver.r", "--wd="+out_folder]
+            time[opaver_r]()
 
