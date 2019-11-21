566fa8ff881c10214d3e4d2cd51249278ad85acf
Migun Shakya <microbeatic@gmail.com>
2019-11-21 11:33:21 -0700

    fixing count test with new path

--- tests/test_featurecount.py
+++ tests/test_featurecount.py
@@ -3,17 +3,31 @@
 import os
 import pytest
 import luigi
-from piret.Runs import Summ
-from plumbum.cmd import rm
+from luigi.interface import build
+from piret.counts import featurecounts as fc
+from plumbum.cmd import rm, cp
 
 
+def test_featurecount():
+    """Test star index creation and mapping."""
 
-def test_star():
-        """Test star index creation and mapping."""
-        luigi.interface.build([featurecount.FeatureCountsII(gff="tests/data2/chr22_ERCC92.gff3",
-                               num_cpus=1, stranded=1, out_dir="tests/test_featurecount",
-                      bam_list=["tests/data2/chr22.bam", "tests/data2/chr22.bam"])], local_scheduler=True)
-        assert os.path.exists("tests/test_featurecount/gene_count.tsv") is True
+    map_dir = os.path.join("tests/test_count", "processes", "mapping", "samp5")
+    if os.path.exists(map_dir) is False:
+        os.makedirs(map_dir)
+    cp_cmd = ["tests/data/samp5_srt.bam", map_dir]
+    cp[cp_cmd]()
+    
+    build([fc.FeatureCounts(fastq_dic={'samp5':''},
+                            kingdom="prokarya",
+                            gff_file="tests/data/test_prok.gff",
+                            workdir="tests/test_count",
+                            indexfile="",
+                            num_cpus=2,
+                            ref_file="tests/data/test_prok.fna",
+                            fid="ID",
+                            stranded=0)],
+                local_scheduler=True)
+    assert os.path.exists("tests/test_count/processes/featureCounts/prokarya/gene_count.tsv") is True
 
-        rm_cmd = rm["-rf", "tests/test_star_mapping"]
-        rm_cmd()
+    rm_cmd = rm["-rf", "tests/test_count"]
+    rm_cmd()
