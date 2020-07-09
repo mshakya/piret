5f7a726576fd8629ce90c9a9007d86df62fa6428
Migun Shakya <microbeatic@gmail.com>
2020-07-09 17:14:55 -0600

    formatting

--- piret/workflows/dual_seq.py
+++ piret/workflows/dual_seq.py
@@ -1,3 +1,18 @@
+from luigi.interface import build
+from piret.pathways import opaver
+from piret.functions import function
+from piret.summary import summarize
+from piret.counts import stringtie
+from piret.dge import ballgown
+from piret.dge import DESeq2
+from piret.dge import edgeR
+from piret.counts import featurecounts
+from piret.maps import star
+from piret.novel import srna
+from piret.runs import Map
+from piret.qc import FaQC
+from piret.maps import hisat2
+from piret.checks.design import CheckDesign
 import os
 import sys
 import argparse
@@ -7,22 +22,6 @@ lib_path = os.path.abspath(os.path.join(dir_path, '..'))
 bin_path = os.path.join(lib_path, 'bin')
 sys.path.append(lib_path)
 os.environ["PATH"] += os.pathsep + bin_path
-from piret.checks.design import CheckDesign
-from piret.maps import hisat2
-from piret.qc import FaQC
-from piret.runs import Map
-from piret.novel import srna
-from piret.maps import star
-from piret.counts import featurecounts
-from piret.dge import edgeR
-from piret.dge import DESeq2
-from piret.dge import ballgown
-from piret.counts import stringtie
-from piret.summary import summarize
-from piret.functions import function
-from piret.pathways import opaver
-from luigi.interface import build
-
 
 
 class DualSeq:
@@ -40,7 +39,7 @@ class DualSeq:
         self.euk_gff = kwargs['euk_gff']
         self.exp_desn_file = kwargs['exp_desn_file']
         self.pathway = kwargs["pathway"]
-        self.hisat_index = kwargs["hisat_index"]        
+        self.hisat_index = kwargs["hisat_index"]
         self.kingdom = kwargs["kingdom"]
         self.local_scheduler = kwargs["local_scheduler"]
         self.p_value = kwargs["p_value"]
@@ -61,7 +60,7 @@ class DualSeq:
             for samp, path in self.fastq_dic.items():
                 trim_dir = os.path.join(self.workdir, "processes", "qc", samp)
                 qc_dic[samp] = trim_dir + "/" + samp + ".1.trimmed.fastq" + ":" + \
-                               trim_dir + "/" + samp + ".2.trimmed.fastq"
+                    trim_dir + "/" + samp + ".2.trimmed.fastq"
             return qc_dic
 
     def create_db(self):
@@ -72,92 +71,92 @@ class DualSeq:
                                      hi_index=self.hisat_index,
                                      num_cpus=self.num_cpus),
                    Map.SAMindex(fasta=ref_fastas, workdir=self.workdir)],
-                #    Map.CreateSplice(gff_file=self.ref_gffs.split(",")[1],
-                            # workdir=self.workdir),
-                #    Map.GetChromName(prok_ref=self.ref_fastas.split(",")[0],
-                            # euk_ref=self.ref_fastas.split(",")[1],
-                            # workdir=self.workdir)],
-                          local_scheduler=self.local_scheduler,
-                          workers=1)
+                  #    Map.CreateSplice(gff_file=self.ref_gffs.split(",")[1],
+                  # workdir=self.workdir),
+                  #    Map.GetChromName(prok_ref=self.ref_fastas.split(",")[0],
+                  # euk_ref=self.ref_fastas.split(",")[1],
+                  # workdir=self.workdir)],
+                  local_scheduler=self.local_scheduler,
+                  workers=1)
         elif self.aligner in ["STAR", "star"]:
             ref_fastas = self.prok_fasta + "," + self.euk_fasta
             build([star.STARindex(fasta=ref_fastas,
-                                num_cpus=self.num_cpus,
-                                gff_file=self.euk_gff,
-                                stardb_dir = self.stardb_dir,
-                                kingdom=self.kingdom),
+                                  num_cpus=self.num_cpus,
+                                  gff_file=self.euk_gff,
+                                  stardb_dir=self.stardb_dir,
+                                  kingdom=self.kingdom),
                    Map.SAMindex(fasta=ref_fastas, workdir=self.workdir)
-                #    Map.CreateSplice(gff_file=self.euk_gff,
-                #             workdir=self.workdir),
-                #    Map.GetChromName(prok_ref=self.prok_fasta,
-                #                     euk_ref=self.euk_fasta,
-                #             workdir=self.workdir)
-                            ],
-                            local_scheduler=self.local_scheduler)
+                   #    Map.CreateSplice(gff_file=self.euk_gff,
+                   #             workdir=self.workdir),
+                   #    Map.GetChromName(prok_ref=self.prok_fasta,
+                   #                     euk_ref=self.euk_fasta,
+                   #             workdir=self.workdir)
+                   ],
+                  local_scheduler=self.local_scheduler)
 
     def map_reads(self):
         """Function to map reads."""
         if self.aligner == "hisat2":
             build([hisat2.HisatMapW(fastq_dic=self.fastq_dic, num_cpus=self.num_cpus,
-                                 indexfile=self.hisat_index, workdir=self.workdir,
-                                 kingdom=self.kingdom)],
-              local_scheduler=self.local_scheduler)
+                                    indexfile=self.hisat_index, workdir=self.workdir,
+                                    kingdom=self.kingdom)],
+                  local_scheduler=self.local_scheduler)
         elif self.aligner in ["STAR", "star"]:
             build([star.map_starW(fastq_dic=self.fastq_dic, num_cpus=self.num_cpus,
-                                 stardb_dir=self.stardb_dir, workdir=self.workdir)],
+                                  stardb_dir=self.stardb_dir, workdir=self.workdir)],
                   local_scheduler=self.local_scheduler)
 
     def map_summarize(self):
         """Summarize mapped reads into a table."""
         if self.aligner == "hisat2":
             build([hisat2.SummarizeHisatMap(fastq_dic=self.fastq_dic,
-                                    workdir=self.workdir,
-                                    indexfile=self.hisat_index,
-                                    num_cpus=self.num_cpus,
-                                    kingdom=self.kingdom)],
-            local_scheduler=self.local_scheduler, workers=1)
+                                            workdir=self.workdir,
+                                            indexfile=self.hisat_index,
+                                            num_cpus=self.num_cpus,
+                                            kingdom=self.kingdom)],
+                  local_scheduler=self.local_scheduler, workers=1)
         elif self.aligner == "STAR":
             build([star.SummarizeStarMap(fastq_dic=self.fastq_dic,
-                                    workdir=self.workdir,
-                                    stardb_dir=self.stardb_dir,
-                                    num_cpus=self.num_cpus)],
-            local_scheduler=self.local_scheduler, workers=1)
+                                         workdir=self.workdir,
+                                         stardb_dir=self.stardb_dir,
+                                         num_cpus=self.num_cpus)],
+                  local_scheduler=self.local_scheduler, workers=1)
 
     def split_prokeuk(self):
         build([Map.Split2ProkEukW(fastq_dic=self.fastq_dic,
-                                workdir=self.workdir,
+                                  workdir=self.workdir,
                                   ref_file=self.prok_fasta + "," + self.euk_fasta)],
-                          local_scheduler=self.local_scheduler,
-                          workers=1)
+              local_scheduler=self.local_scheduler,
+              workers=1)
 
     def summarize_map(self):
         """Summarize mapped reads into a table."""
         if self.aligner == "hisat2":
             build([Map.hisat2.SummarizeHisatMap(fastq_dic=self.fastq_dic,
-                                    workdir=self.workdir,
-                                    indexfile=self.hisat_index,
-                                    num_cpus=self.num_cpus)],
-            local_scheduler=self.local_scheduler, workers=1)
+                                                workdir=self.workdir,
+                                                indexfile=self.hisat_index,
+                                                num_cpus=self.num_cpus)],
+                  local_scheduler=self.local_scheduler, workers=1)
         elif self.aligner == "STAR":
             build([Map.SummarizeStarMap(fastq_dic=self.fastq_dic,
-                                    workdir=self.workdir,
-                                    stardb_dir=self.stardb_dir,
-                                    num_cpus=self.num_cpus)],
-            local_scheduler=self.local_scheduler, workers=1)
+                                        workdir=self.workdir,
+                                        stardb_dir=self.stardb_dir,
+                                        num_cpus=self.num_cpus)],
+                  local_scheduler=self.local_scheduler, workers=1)
 
     def extract_pp(self):
         """Extract properly paired reads."""
         build([srna.ExtractPPW(fastq_dic=self.fastq_dic, num_cpus=self.num_cpus,
-                                 indexfile=self.hisat_index, workdir=self.workdir,
-                                 kingdom=self.kingdom)],
+                               indexfile=self.hisat_index, workdir=self.workdir,
+                               kingdom=self.kingdom)],
               local_scheduler=self.local_scheduler)
 
     def NovelRegions(self):
         """Find novel regions."""
         build([srna.FindNovelRegionsW(fastq_dic=self.fastq_dic,
-                                  workdir=self.workdir,
-                                 kingdom=self.kingdom,
-                                 gff_file=self.prok_gff + "," + self.euk_gff)],
+                                      workdir=self.workdir,
+                                      kingdom=self.kingdom,
+                                      gff_file=self.prok_gff + "," + self.euk_gff)],
               local_scheduler=self.local_scheduler)
 
     def create_new_gff(self):
@@ -165,30 +164,30 @@ class DualSeq:
                                kingdom=self.kingdom,
                                workdir=self.workdir,
                                gff_file=self.prok_gff + "," + self.euk_gff)],
-          local_scheduler=self.local_scheduler, workers=1)
+              local_scheduler=self.local_scheduler, workers=1)
 
     def merge_stringtie(self, new_gff):
         build([stringtie.MergeStringTies(fastq_dic=self.fastq_dic,
                                          num_cpus=self.num_cpus,
-                                workdir=self.workdir,
-                                gff_file=new_gff,
-                                kingdom=self.kingdom)],
-           local_scheduler=self.local_scheduler, workers=1)
-    
+                                         workdir=self.workdir,
+                                         gff_file=new_gff,
+                                         kingdom=self.kingdom)],
+              local_scheduler=self.local_scheduler, workers=1)
+
     def restringtie(self):
         build([stringtie.ReStringTieScoresW(fastq_dic=self.fastq_dic,
-                                       num_cpus=self.num_cpus,
-                                       workdir=self.workdir,
-                                       kingdom=self.kingdom)],
-           local_scheduler=self.local_scheduler, workers=1)
+                                            num_cpus=self.num_cpus,
+                                            workdir=self.workdir,
+                                            kingdom=self.kingdom)],
+              local_scheduler=self.local_scheduler, workers=1)
 
     def run_ballgown(self):
         build([ballgown.ballgown(
-                            workdir=self.workdir,
-                            kingdom="eukarya",
-                            exp_design=self.exp_desn_file,
-                            p_value=self.p_value)],
-               local_scheduler=self.local_scheduler, workers=1)
+            workdir=self.workdir,
+            kingdom="eukarya",
+            exp_design=self.exp_desn_file,
+            p_value=self.p_value)],
+            local_scheduler=self.local_scheduler, workers=1)
 
     def feature_counts(self, new_gff, kingdom):
         build([featurecounts.FeatureCounts(fastq_dic=self.fastq_dic,
@@ -198,21 +197,21 @@ class DualSeq:
                                            kingdom=kingdom,
                                            workdir=self.workdir,
                                            ref_file=self.prok_fasta + "," + self.euk_fasta)],
-          local_scheduler=self.local_scheduler, workers=1)
+              local_scheduler=self.local_scheduler, workers=1)
 
     def run_edger(self, kingdom):
         build([edgeR.edgeR(workdir=self.workdir,
                            kingdom=kingdom,
                            exp_design=self.exp_desn_file,
                            p_value=self.p_value)], local_scheduler=self.local_scheduler,
-                           workers=1)
+              workers=1)
 
     def run_deseq2(self, kingdom):
         build([DESeq2.DESeq2(workdir=self.workdir,
                              kingdom=kingdom,
                              exp_design=self.exp_desn_file,
                              p_value=self.p_value)],
-        local_scheduler=self.local_scheduler, workers=1)
+              local_scheduler=self.local_scheduler, workers=1)
 
     def run_emapper(self, new_gff, kingdom, fasta):
         build([function.RunEmapper(workdir=self.workdir,
@@ -224,8 +223,8 @@ class DualSeq:
 
     def run_opaver(self, method, kingdom):
         build([opaver.RunOpaver(workdir=self.workdir,
-                         kingdom=kingdom,
-                         method=method)],
+                                kingdom=kingdom,
+                                method=method)],
               local_scheduler=self.local_scheduler, workers=1)
 
     def summ_json(self, new_gff, method, NovelRegions, kingdom, fasta):
