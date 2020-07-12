ff76b74b72cc2d46407abb1bd2ce564608c289b5
Migun Shakya <microbeatic@gmail.com>
2020-07-12 10:59:07 -0600

    linted

--- piret/miscs.py
+++ piret/miscs.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 
 
+from luigi import Parameter
+from luigi import LocalTarget
+from luigi import ExternalTask
 import os
 import sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 lib_path = os.path.abspath(os.path.join(dir_path, '..'))
 sys.path.append(lib_path)
-from luigi import ExternalTask
-from luigi import LocalTarget
-from luigi import Parameter
+
 
 class RefFile(ExternalTask):
     """An ExternalTask like this."""
--- piret/workflows/__init__.py
+++ piret/workflows/__init__.py
@@ -1,10 +1,10 @@
 """Importing."""
 import os
 import sys
+from dual_seq import DualSeq
+from single_seq import SingleSeq
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
 
-from single_seq import SingleSeq
-from dual_seq import DualSeq
 """Importing."""
-
