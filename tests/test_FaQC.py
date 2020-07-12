a92df1868f3eee8b26eaab580f4986508388f4ac
Migun Shakya <microbeatic@gmail.com>
2020-07-11 23:55:56 -0600

    replaced unittest with pytest

--- tests/test_FaQC.py
+++ tests/test_FaQC.py
@@ -1,55 +1,56 @@
 #! /usr/bin/env python
 
+
 import sys
 import os
 import unittest
 import shutil
 from luigi.interface import build
+from piret.qc import FaQC
 DIR = os.path.dirname(os.path.realpath(__file__))
 LIB = os.path.abspath(os.path.join(DIR, '..'))
 sys.path.append(LIB)
-from piret.qc import FaQC
 
 
+def test_faqc_function():
+    """Test faqc."""
+    fq_path = os.path.join("test_faqc", "processes", "qc", "samp1")
+    if os.path.exists(fq_path) is False:
+        os.makedirs(fq_path)
+    build([FaQC.PairedRunQC(fastqs=["tests/data/fastqs/BTT_test15_R1.fastq.gz",
+                                    "tests/data/fastqs/BTT_test15_R2.fastq.gz"],
+                            sample="samp1",
+                            num_cpus=1,
+                            workdir="test_faqc",
+                            faqc_min_L=50,
+                            avg_q=20,
+                            n_cutoff=5)],
+          local_scheduler=True, workers=1)
+
+    assert os.stat(os.path.join("test_faqc", "processes", "qc", "samp1",
+                                "samp1.1.trimmed.fastq")).st_size > 1
+
+    # rm_cmd = rm["-rf", "tests/data/test_prok/processes/DESeq2/prokarya/gene/spleen__over__liver__gene__sig.csv"]
+    # rm_cmd()
 
 
-class TestFaQC(unittest.TestCase):
-    """Unittest testcase."""
-
-    def file_len(self, fname):
-        """A function to test filename"""
-        with open(fname) as f:
-            for i, l in enumerate(f):
-                pass
-        return i + 1
-
-    def test_faqc_function(self):
-        """Test FaQC function."""
-        dirpath = os.path.dirname(os.path.realpath(__file__))
-        build([FaQC.PairedRunQC(fastqs=["tests/data/fastqs/BTT_test15_R1.fastq.gz",
-                                        "tests/data/fastqs/BTT_test15_R1.fastq.gz"],
-                                sample="samp1",
-                                num_cpus=1,
-                                qc_outdir="test_faqc",
-                                faqc_min_L=50,
-                                avg_q=20,
-                                n_cutoff=5)],
-              local_scheduler=True, workers=1)
-
-    def test_fq_out(self):
-        """Test if fq has expected line number"""
-        fq_lines = self.file_len("test_faqc/samp1.1.trimmed.fastq")
-        assert(fq_lines) == 189508
-
-    def test_output(self):
-        """Test if 4 files are generated as output"""
-        filenames = [x[2] for x in os.walk("test_faqc")][0]
-        assert len(filenames) == 4
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree('test_faqc')
+def test_fq_out():
+    """Test if fq has expected line number"""
+    fq_lines = file_len(
+        "test_faqc/processes/qc/samp1/samp1.1.trimmed.fastq")
+    assert(fq_lines) == 177748
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_output():
+    """Test if 5 files are generated as output"""
+    filenames = [x[2] for x in os.walk("test_faqc/processes/qc/samp1")][0]
+    assert len(filenames) == 5
+    shutil.rmtree('test_faqc')
+
+
+def file_len(fname):
+    """A function to test filename"""
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
