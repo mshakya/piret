2971ac598c284ccd31ee279822a755fa2f3fe331
Migun Shakya <microbeatic@gmail.com>
2020-10-21 09:28:44 -0600

    linting

--- piret/miscs.py
+++ piret/miscs.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 
 
+import os
+import sys
 from luigi import Parameter
 from luigi import LocalTarget
 from luigi import ExternalTask
-import os
-import sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 lib_path = os.path.abspath(os.path.join(dir_path, '..'))
 sys.path.append(lib_path)
