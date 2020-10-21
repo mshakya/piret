0d64c8e1502ab067ac6168bb94e0f487bb90019e
Migun Shakya <microbeatic@gmail.com>
2020-10-21 09:26:58 -0600

    adding a TODO

--- piret/checks/gff3.py
+++ piret/checks/gff3.py
@@ -7,6 +7,9 @@ import re
 import collections
 import pandas as pd
 
+# TODO featurecount throws an error when semicolon numbers are inconsistent, so fix gff or check and print an error
+
+
 class CheckGFF():
     """Check experimental design are tab delimited."""
 
@@ -15,6 +18,7 @@ class CheckGFF():
         self.gff_col = pd.read_csv(gff_file, sep='\t', comment="#")
         self.gff_col.columns = ["seqid", "source", "type", "start", "end",
                                 "score", "strand", "phase", "attributes"]
+
     def size(self):
         """Check if gff file has 9 columns."""
         if len(self.gff_col.columns) != 9:
@@ -25,7 +29,8 @@ class CheckGFF():
 
     def check_id(self):
         """Check if every row has ID in the 9th column."""
-        gffw_child = self.gff_col.loc[self.gff_col['type'].isin(["mRNA", "gene", "CDS", "exon"])]
+        gffw_child = self.gff_col.loc[self.gff_col['type'].isin(
+            ["mRNA", "gene", "CDS", "exon"])]
         nrows = gffw_child.shape[0]
         id_count = gffw_child.iloc[:, 8].str.contains('ID=').sum()
         if id_count == nrows:
@@ -54,11 +59,13 @@ class CheckGFF():
             gff_cds.loc[gff_cds.index, 'cds_ID'] = cds_id
             cds_ids = gff_cds['cds_ID'].tolist()
         if gff_gene.empty is False:
-            gene_id = gff_gene.apply(lambda row: p_reg.search(row['attributes']).group(0), axis=1)
+            gene_id = gff_gene.apply(lambda row: p_reg.search(
+                row['attributes']).group(0), axis=1)
             gff_gene.loc[gff_gene.index, 'gene_ID'] = gene_id
             gene_ids = gff_gene['gene_ID'].tolist()
         if gff_exon.empty is False:
-            exon_id = gff_exon.apply(lambda row: p_reg.search(row['attributes']).group(0), axis=1)
+            exon_id = gff_exon.apply(lambda row: p_reg.search(
+                row['attributes']).group(0), axis=1)
             gff_exon.loc[gff_exon.index, 'exon_ID'] = exon_id
             exon_ids = gff_exon['exon_ID'].tolist()
         if gff_mrna.empty is False and gff_cds.empty is False:
@@ -86,7 +93,7 @@ class CheckGFF():
                 print(set(cds_ids).intersection(gene_ids))
                 sys.exit("""Found overlapping IDs in gene and CDS \n
                         see: https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md""")
-        if gff_exon.empty is False and gff_cds.empty is False:            
+        if gff_exon.empty is False and gff_cds.empty is False:
             if len(list(set(exon_ids).intersection(cds_ids))) > 0:
                 print(set(exon_ids).intersection(cds_ids))
                 sys.exit("""Found overlapping IDs in exon and CDS \n
